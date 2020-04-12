31d13f360416bdcc92186e6a7ae8f28bb145e02b
sgalvan <sakura-lienzo@hotmail.com>
2020-04-12 17:54:04 -0500

    more about scope in js

---
+++ src/scope/block.js
@@ -0,0 +1,15 @@
+const fruits = () => {
+    if (true){
+        var f1 = 'apple'
+        var f2 = 'pineaple'
+        let f3 = 'banana'
+        const f4 = 'orange'
+    }
+    console.log(f1)
+    console.log(f2)
+    // console.log(f3)
+    // console.log(f4)
+
+}
+
+fruits()
